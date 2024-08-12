85ce4755c008994b8b6670bf7539d753f6bc832a
Yuki-0828 <2318528303@qq.com>
2024-08-12 16:24:25 +0800

    提交

---
+++ build/CMakeFiles/Factory_main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Factory_main.dir/Factory.cpp.obj"
+  "CMakeFiles/Factory_main.dir/Factory.cpp.obj.d"
+  "CMakeFiles/Factory_main.dir/main.cpp.obj"
+  "CMakeFiles/Factory_main.dir/main.cpp.obj.d"
+  "Factory_main.exe"
+  "Factory_main.exe.manifest"
+  "Factory_main.pdb"
+  "libFactory_main.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Factory_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
